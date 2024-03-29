dd7220c16909898322a4295dffe5b0d673242524
Knut H Pedersen <knuthp@gmail.com>
2024-03-30 00:19:46 +0100

    Explorative vehicle monitoring
    Bin delays and add colors
    Add more popup data
    Filter not updated vehicles

--- spartid_pubtransport/stapp/stapp.py
+++ spartid_pubtransport/stapp/stapp.py
@@ -1,48 +1,101 @@
 import folium
+import pandas as pd
 import streamlit as st
+from folium.plugins import Fullscreen
 from streamlit_folium import folium_static
 
 from spartid_pubtransport import vehiclemonitoring
 
+MAP__CENTER__LONG = 10.611268532369081
+MAP__CENTER__LAT = 59.669607206900906
 
-@st.cache_data(ttl=5*60)
+
+@st.cache_data(ttl=5 * 60)
 def get_vm():
     return vehiclemonitoring.get_vehicles()
-    
 
 
+def _df_bin_delays(df: pd.DataFrame) -> pd.DataFrame:
+    bins = [
+        pd.Timedelta(days=-1),
+        pd.Timedelta(minutes=0),
+        pd.Timedelta(minutes=5),
+        pd.Timedelta(minutes=10),
+        pd.Timedelta(minutes=20),
+        pd.Timedelta(minutes=30),
+        pd.Timedelta(days=4),
+    ]
+
+    labels = ["unreasonable", "0-5min", "5-10min", "10-20min", "20-30min", "30min+"]
+    colors = ["gray", "green", "blue", "orange", "red", "darkred"]
+    color_map = {label: color for label, color in zip(labels, colors)}
+
+    return df.assign(
+        delay_bin=pd.cut(df["Delay"], bins, labels=labels),
+    ).assign(delay_color=lambda df1: df1.delay_bin.map(color_map))
+
+
+st.set_page_config(layout="wide")
 st.title("Spartid Public Transport")
 
-geo_df = get_vm()
+geo_df_raw = get_vm()
+geo_df = geo_df_raw.pipe(_df_bin_delays).assign(
+    icon=geo_df_raw["VehicleMode"].map(
+        {
+            "unknown": "question-circle",
+            "bus": "bus",
+            "ferry": "ship",
+            "rail": "train",
+        }
+    ),
+)
 
-center = geo_df.dissolve().centroid
+st.write(f"Monitoring: {len(geo_df_raw)} vehicles")
 
-map = folium.Map(location=[center.y, center.x], zoom_start=4)
+vehicle_modes = geo_df["VehicleMode"].unique()
 
-geo_df_list = [[point.xy[1][0], point.xy[0][0]] for point in geo_df.geometry]
+st.number_input("Hours since last update:", min_value=0, max_value=24, value=1)
+one_hour = pd.Timestamp.now(tz="Europe/Berlin") - pd.Timedelta(hours=1)
 
-for i, coordinates in enumerate(geo_df_list):
-    if geo_df["VehicleMode"].fillna('')[i] == "":
-        type_color = "black"
-    elif geo_df["VehicleMode"][i] == "bus":
-        type_color = "green"
-    elif geo_df["VehicleMode"][i] == "ferry":
-        type_color = "red"
-    elif geo_df["VehicleMode"][i] == "rail":
-        type_color = "yellow"
-    else:
-        type_color = "black"
+limit_to_types = st.multiselect(
+    label="Type",
+    options=vehicle_modes,
+    default=list(vehicle_modes),
+)
+
+geo_df_filteres = geo_df.query("VehicleMode in @limit_to_types").query(
+    "RecordedAtTime > @one_hour"
+)
+st.write(f"{len(geo_df)} total filtere {len(geo_df_filteres)}")
 
 
+map = folium.Map(location=[MAP__CENTER__LAT, MAP__CENTER__LONG], zoom_start=8)
+Fullscreen(position="topleft").add_to(map)
+
+for index, row in geo_df_filteres.iterrows():
+    coordinates = [row["Latitude"], row["Longitude"]]
     # Place the markers with the popup labels and data
     map.add_child(
         folium.Marker(
             location=coordinates,
-            popup="LineRef: "
-            + str(geo_df["LineRef"][i])
-            + "<br>",
-            icon=folium.Icon(color="%s" % type_color),
+            popup=f"""
+            <table>
+            <tr><th>LineRef:</th><td>{str(row["LineRef"])}</td></tr>
+            <tr><th>Delay:</th><td>{str(row["Delay"])}</td></tr>
+            <tr><th>Delay:</th><td>{str(row["delay_bin"])}{str(row["delay_color"])}</td></tr>
+            <tr><th>OriginName:</th><td>{str(row["OriginName"])}</td></tr>
+            <tr><th>DestinationName:</th><td>{str(row["DestinationName"])}</td></tr>
+            <tr><th>VehicleStatus:</th><td>{str(row["VehicleStatus"])}</td></tr>
+            <tr><th>OriginAimedDepartureTime:</th><td>{str(row["OriginAimedDepartureTime"])}</td></tr>
+            </table>
+            """,
+            icon=folium.Icon(
+                icon=row["icon"], prefix="fa", color=f"{row["delay_color"]}"
+            ),
         )
     )
 
 folium_static(map)
+
+st.header("Data")
+st.dataframe(geo_df.drop(columns="geometry"))
--- spartid_pubtransport/vehiclemonitoring.py
+++ spartid_pubtransport/vehiclemonitoring.py
@@ -1,48 +1,83 @@
-import requests
-import pandas as pd
-
 import geopandas
+import pandas as pd
+import requests
 from lxml import etree
-
 
 base_url = "https://api.entur.io/realtime/v1/rest/vm"
 nsmap = {
-    'siri': 'http://www.siri.org.uk/siri',
-    'ns2': "http://www.ifopt.org.uk/acsb",
+    "siri": "http://www.siri.org.uk/siri",
+    "ns2": "http://www.ifopt.org.uk/acsb",
     "ns3": "http://www.ifopt.org.uk/ifopt",
     "ns4": "http://datex2.eu/schema/2_0RC1/2_0",
 }
 
-def get_vehicles():
-    resp = requests.get(f"{base_url}?maxSize=100000")
+session = requests.Session()
+session.headers.update({"ET-Client-Name": "knuthp-spartid-pubtransport"})
+
+
+def get_vehicles() -> geopandas.GeoDataFrame:
+    """Gets Entur SIRI Vehicle Monitoring as geopandas GeoDataFrame.
+
+    Transforms SIRI XML to pandas DataFrame.
+    Cleans up datatypes
+    Returns as Geopandas GeoDataframe
+    """
+    resp = session.get(f"{base_url}?maxSize=100000")
     assert resp.ok
-    prstree = etree.fromstring(resp.content) 
+    df_raw = _siri_mv_to_df_raw(resp)
+    df = _df_raw_to_clean(df_raw)
+
+    return geopandas.GeoDataFrame(
+        df,
+        geometry=geopandas.points_from_xy(df.Longitude, df.Latitude),
+        crs="EPSG:4326",
+    )
+
+
+def _df_raw_to_clean(df_raw: pd.DataFrame) -> pd.DataFrame:
+    return df_raw.assign(
+        VehicleMode=df_raw.VehicleMode.fillna("unknown"),
+        RecordedAtTime=pd.to_datetime(df_raw.RecordedAtTime, format="ISO8601"),
+        ValidUntilTime=pd.to_datetime(
+            df_raw.ValidUntilTime, format="ISO8601", errors="coerce"
+        ),
+        Bearing=df_raw.Bearing.astype("float32[pyarrow]"),
+        Delay=pd.to_timedelta(df_raw.Delay),
+    )
+
+
+def _siri_mv_to_df_raw(resp):
+    prstree = etree.fromstring(resp.content)
 
     all_items = []
     for vehicle_activity in prstree.iter(etree.QName(nsmap["siri"], "VehicleActivity")):
-        vehicle_activity_dict = {etree.QName(x).localname : x.text for x in vehicle_activity if x.text is not None}
-        monitored_journey = vehicle_activity.find(etree.QName(nsmap["siri"], "MonitoredVehicleJourney"))
-        monitored_journey_dict = {etree.QName(x).localname : x.text for x in monitored_journey if x.text is not None}
-        vehicle_location = monitored_journey.find(etree.QName(nsmap["siri"], "VehicleLocation"))
+        vehicle_activity_dict = {
+            etree.QName(x).localname: x.text
+            for x in vehicle_activity
+            if x.text is not None
+        }
+        monitored_journey = vehicle_activity.find(
+            etree.QName(nsmap["siri"], "MonitoredVehicleJourney")
+        )
+        monitored_journey_dict = {
+            etree.QName(x).localname: x.text
+            for x in monitored_journey
+            if x.text is not None
+        }
+        vehicle_location = monitored_journey.find(
+            etree.QName(nsmap["siri"], "VehicleLocation")
+        )
         if vehicle_location is not None:
-            vehicle_location_dict = {etree.QName(x).localname : x.text for x in vehicle_location if x.text is not None}
+            vehicle_location_dict = {
+                etree.QName(x).localname: x.text
+                for x in vehicle_location
+                if x.text is not None
+            }
         else:
             vehicle_location = {}
 
-        all_items.append(vehicle_activity_dict | monitored_journey_dict | vehicle_location_dict)
-
-    df_raw = pd.DataFrame(all_items).convert_dtypes(dtype_backend="pyarrow")
-
-    df = (df_raw
-        .assign(
-            # RecordedAtTime=pd.to_datetime(df_raw.RecordedAtTime, format="ISO8601"),
-            # ValidUntilTime=pd.to_datetime(df_raw.ValidUntilTime, format="ISO8601", errors="coerce"),
-            # Bearing=df_raw.Bearing.astype("float32[pyarrow]"),
-        #  Delay=pd.to_timedelta(df_raw.Delay),
+        all_items.append(
+            vehicle_activity_dict | monitored_journey_dict | vehicle_location_dict
         )
-    )
 
-    gdf = geopandas.GeoDataFrame(
-        df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4326"
-    )
-    return gdf
+    return pd.DataFrame(all_items).convert_dtypes(dtype_backend="pyarrow")
